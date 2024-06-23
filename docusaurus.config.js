167fc029c5ad7374ad3348f27c5fcab926a93b7c
Atlas Fyber <78761344+atlasfyber@users.noreply.github.com>
2024-06-24 00:59:54 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://horizonbot.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/horizon/',
+  baseUrl: '/horizon-website/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
