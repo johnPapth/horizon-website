7bb74b81db8710933c336614cb9ad1229ed9a195
Atlas Fyber <78761344+atlasfyber@users.noreply.github.com>
2024-06-26 00:12:53 +0300

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,6 @@ const config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
-
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'AtlasDevGroup', // Usually your GitHub org/user name.
@@ -45,7 +44,6 @@ const config = {
         },
         blog: {
           showReadingTime: true,
-
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -54,101 +52,111 @@ const config = {
     ],
   ],
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      // Replace with your project's social card
+  themeConfig: {
     colorMode: {
       defaultMode: 'dark',
       disableSwitch: true,
       respectPrefersColorScheme: false,
     },
-      announcementBar: {
-        id: 'support_us',
-        content:
-          'Check out the official Horizon APP <a target="_blank" rel="noopener noreferrer" href="https://discord.com/application-directory/1183177251316047983">Discord Discovery</a> Page',
-        backgroundColor: '#272728',
-        textColor: '#fafbfc',
-        isCloseable: true,
+    announcementBar: {
+      id: 'support_us',
+      content:
+        'Check out the official Horizon APP <a target="_blank" rel="noopener noreferrer" href="https://discord.com/application-directory/1183177251316047983">Discord Discovery</a> Page',
+      backgroundColor: '#272728',
+      textColor: '#fafbfc',
+      isCloseable: true,
+    },
+    image: 'img/header.jpg',
+    navbar: {
+      logo: {
+        alt: 'Horizon Logo',
+        src: 'img/logo.png',
       },
-      image: 'img/header.jpg',
-      navbar: {
-        logo: {
-          alt: 'Horizon Logo',
-          src: 'img/logo.png',
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
         },
-        items: [
-          {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Documentation',
-          },
-          
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://donate.teamatlas.dev',
-            label: 'Donate',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Horizon',
-            items: [
-              {
-                label: 'Documentation',
-                to: '/docs/welcome',
-              },
-              {
-                label: 'Status Page',
-                href: 'https://status.teamatlas.dev',
-              },
-              {
-                label: 'Vote Horizon',
-                href: 'https://top.gg/bot/1183177251316047983/vote',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Discord',
-                href: 'https://support.horizonbot.xyz',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://x.horizonbot.xyz',
-              },
-            ],
-          },
-          {
-            title: 'Terms',
-            items: [
-              {
-                label: 'Terms of Service',
-                to: '/terms',
-              },
-              {
-                label: 'Privacy Policy',
-                to: '/privacy',
-              },
-
-            ],
-          },
-          
-        ],
-        copyright: `© ${new Date().getFullYear()} Horizon. All rights reserved. Not affiliated with Discord Inc.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    }),
+        { to: '/blog', label: 'Blog', position: 'left' },
+        {
+          href: 'https://donate.teamatlas.dev',
+          label: 'Donate',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Horizon',
+          items: [
+            {
+              label: 'Documentation',
+              to: '/docs/welcome',
+            },
+            {
+              label: 'Status Page',
+              href: 'https://status.teamatlas.dev',
+            },
+            {
+              label: 'Vote Horizon',
+              href: 'https://top.gg/bot/1183177251316047983/vote',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Discord',
+              href: 'https://support.horizonbot.xyz',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://x.horizonbot.xyz',
+            },
+          ],
+        },
+        {
+          title: 'Legal',
+          items: [
+            {
+              label: 'Terms of Service',
+              to: '/terms',
+            },
+            {
+              label: 'Privacy Policy',
+              to: '/privacy',
+            },
+          ],
+        },
+        // New category added to the left
+        {
+          title: 'Top.gg Stats',
+          items: [
+            {
+              html: `
+              <a href="https://top.gg/bot/1183177251316047983">
+                <img src="https://top.gg/api/widget/upvotes/1183177251316047983.svg">
+              </a> <br>
+              <a href="https://top.gg/bot/1183177251316047983">
+                  <img src="https://top.gg/api/widget/owner/1183177251316047983.svg">
+              </a>
+              `,
+            },
+          ],
+        },
+      ],
+      copyright: `© ${new Date().getFullYear()} Horizon. All rights reserved. Not affiliated with Discord Inc.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  },
 };
 
 export default config;
