9def252aca751eca6f507bb17b91f1d42bad927c
Atlas Fyber <78761344+atlasfyber@users.noreply.github.com>
2024-06-25 16:37:51 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,8 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
-
-// Import the appbadge.png image
 import appBadgeImage from '@site/static/img/appbadge.png';
 
 function HomepageHeader() {
@@ -20,13 +17,14 @@ function HomepageHeader() {
         <div className="hero-content">
           <div className={styles.hero__title}>
             <Heading as="h1" className={styles.hero__title_text}>
-              Welcome to Horizon <img src={appBadgeImage} alt="Horizon Badge" className={styles.hero__badge} />
+              Welcome to Horizon{' '}
+              <img src={appBadgeImage} alt="Horizon Badge" className={styles.hero__badge} />
             </Heading>
           </div>
           <p className={styles.hero__subtitle}>{siteConfig.tagline}</p>
           <div className={styles.buttons}>
             <Link
-              className="button button--secondary button--lg"
+              className={clsx('button', 'button--secondary', 'button--lg', styles.buttonZoom)}
               href="https://invite.horizonbot.xyz"
             >
               Start exploring the unknown! 🚀
@@ -44,7 +42,7 @@ export default function Home() {
   return (
     <Layout
       title={`Welcome`}
-      description="Horizon offers a variety of space-related commands for your server using real-time data directly on your Discord server! <head />"
+      description="Horizon offers a variety of space-related commands for your server using real-time data directly on your Discord server!"
     >
       <HomepageHeader />
       <main>
@@ -54,4 +52,3 @@ export default function Home() {
     </Layout>
   );
 }
-
